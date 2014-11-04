96924920543ed22adb74bdff3a1c9a2fad2536d2
Keith McDonald <keith.mcdonald@centare.com>
2014-11-04 09:36:50 -0600

    Added package name info logging

--- METOpenLink/src/main/java/com/milwaukeetool/mymilwaukee/activity/MainActivity.java
+++ METOpenLink/src/main/java/com/milwaukeetool/mymilwaukee/activity/MainActivity.java
@@ -3,6 +3,7 @@ package com.milwaukeetool.mymilwaukee.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -38,6 +39,8 @@ public class MainActivity extends Activity {
         getActionBar().hide();
 
         setContentView(R.layout.activity_main);
+
+        Log.i(TAG,"PACKAGE NAME: " + getApplicationContext().getPackageName());
 
         mCreateAccountBtn = (MTButton)findViewById(R.id.createAccountButton);
         mCreateAccountBtn.setOnClickListener(new View.OnClickListener() {
