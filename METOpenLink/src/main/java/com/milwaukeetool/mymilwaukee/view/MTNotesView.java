647025ae911ab60444648ea02108d8c6f57c09ea
Keith McDonald <keith.mcdonald@centare.com>
2014-12-10 11:46:41 -0600

    Updates for issue with multi line text in notes field

--- METOpenLink/src/main/java/com/milwaukeetool/mymilwaukee/view/MTNotesView.java
+++ METOpenLink/src/main/java/com/milwaukeetool/mymilwaukee/view/MTNotesView.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.text.Editable;
 import android.text.InputType;
 import android.view.LayoutInflater;
+import android.view.inputmethod.EditorInfo;
 import android.widget.LinearLayout;
 
 import com.milwaukeetool.mymilwaukee.R;
@@ -14,8 +15,6 @@ import com.milwaukeetool.mymilwaukee.R;
 public class MTNotesView extends LinearLayout {
     private MTEditText mNotes;
 
-    private String mPreviousNotesText = null;
-
     public MTNotesView(Activity activity) {
         super(activity);
 
@@ -24,27 +23,10 @@ public class MTNotesView extends LinearLayout {
         LayoutInflater.from(activity).inflate(R.layout.view_notes, this);
 
         this.mNotes = (MTEditText) findViewById(R.id.itemDetailNotes);
-        this.mNotes.setInputType(InputType.TYPE_TEXT_FLAG_CAP_SENTENCES|InputType.TYPE_TEXT_FLAG_MULTI_LINE);
-
-//        this.mNotes.addTextChangedListener(new TextWatcher() {
-//
-//            @Override
-//            public void beforeTextChanged(CharSequence charSequence, int i, int i2, int i3) {
-//                mPreviousNotesText = charSequence.toString();
-//            }
-//
-//            @Override
-//            public void onTextChanged(CharSequence charSequence, int i, int i2, int i3) {
-//
-//            }
-//
-//            @Override
-//            public void afterTextChanged(Editable s) {
-//                if (s.length() > 256) {
-//                    mNotes.setText(mPreviousNotesText);
-//                }
-//            };
-//        });
+        this.mNotes.setInputType(InputType.TYPE_CLASS_TEXT |
+                InputType.TYPE_TEXT_FLAG_MULTI_LINE |
+                InputType.TYPE_TEXT_FLAG_CAP_SENTENCES);
+        this.mNotes.setImeOptions(EditorInfo.IME_ACTION_NONE);
     }
 
     public String getNotes() {
